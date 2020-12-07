376efe04a9b8475de023d8dcc48beec44893d357
Brigid C <b@c.com>
2020-12-06 20:09:13 -0600

    created two functions, nameInput and fuel

--- script.js
+++ script.js
@@ -7,11 +7,22 @@ window.addEventListener("load", function () {
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
 
-      if (pilotNameInput.value === "" || copilotNameInput.value === "") {
-         alert("All fields are required!");
-      } 
+      nameInput(pilotNameInput, copilotNameInput); 
+      fuel(fuelLevelInput, cargoMassInput);
    });
 });
+
+function nameInput(pilotNameInput, copilotNameInput) {
+   if (pilotNameInput.value === "" || copilotNameInput.value === "") {
+      alert("All fields are required!");
+   }
+}
+
+function fuel(fuelLevelInput, cargoMassInput) {
+   if (isNaN(fuelLevelInput) || isNaN(cargoMassInput)) {
+         alert("Invalid entry. Please enter a number!")
+   }
+};
 
 
 //obtain a reference to the button element
